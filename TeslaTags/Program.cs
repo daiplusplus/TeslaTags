be6d9a5d5e1ef73cdc9f4b64f9a7b1b109ca5941
Dai <dai@dai.me>
2018-05-22 09:09:10 -0700

    New project, Taglib added.

--- TeslaTags/Program.cs
+++ TeslaTags/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace TeslaTags
 {
-	class Program
+	public static class Program
 	{
-		static void Main(string[] args)
+		public static void Main(String[] args)
 		{
+
 		}
 	}
 }
